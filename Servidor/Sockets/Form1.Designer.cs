9110faee0853decb2d4b6064f64f1456e22f1903
urommel <rulco@unitru.edu.pe>
2023-09-16 11:42:35 -0500

    initial commit

---
+++ Servidor/Sockets/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Sockets
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.getHostName = new System.Windows.Forms.Button();
+            this.txtHostName = new System.Windows.Forms.TextBox();
+            this.txtNeighboringIP = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // getHostName
+            // 
+            this.getHostName.Cursor = System.Windows.Forms.Cursors.Default;
+            this.getHostName.Location = new System.Drawing.Point(34, 67);
+            this.getHostName.Name = "getHostName";
+            this.getHostName.Size = new System.Drawing.Size(144, 38);
+            this.getHostName.TabIndex = 0;
+            this.getHostName.Text = "OPTENER NOMBRE DEL HOST";
+            this.getHostName.UseVisualStyleBackColor = true;
+            this.getHostName.Click += new System.EventHandler(this.getHostName_Click);
+            // 
+            // txtHostName
+            // 
+            this.txtHostName.Location = new System.Drawing.Point(283, 111);
+            this.txtHostName.Name = "txtHostName";
+            this.txtHostName.Size = new System.Drawing.Size(130, 20);
+            this.txtHostName.TabIndex = 1;
+            // 
+            // txtNeighboringIP
+            // 
+            this.txtNeighboringIP.Location = new System.Drawing.Point(283, 67);
+            this.txtNeighboringIP.Name = "txtNeighboringIP";
+            this.txtNeighboringIP.Size = new System.Drawing.Size(130, 20);
+            this.txtNeighboringIP.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 225);
+            this.Controls.Add(this.txtNeighboringIP);
+            this.Controls.Add(this.txtHostName);
+            this.Controls.Add(this.getHostName);
+            this.Name = "Form1";
+            this.Text = "Servidor Socket";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button getHostName;
+        private System.Windows.Forms.TextBox txtHostName;
+        private System.Windows.Forms.TextBox txtNeighboringIP;
+    }
+}
+
---
+++ Servidor/Sockets/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Sockets
+{
+    public partial class Form1 : Form
+    {
+
+        IPHostEntry NeighboringHost;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void getHostName_Click(object sender, EventArgs e)
+        {
+            //string HostName = Dns.GetHostName();
+            //txtHostName.Text = HostName;
+
+            String IPLiteral = txtNeighboringIP.Text.Trim();
+
+            IPAddress IPAddress = IPAddress.Parse(IPLiteral);
+
+            NeighboringHost = Dns.GetHostEntry(IPAddress);
+
+            String LiteralHostName = NeighboringHost.HostName;
+
+            txtHostName.Text = LiteralHostName;
+        }
+    }
+}
